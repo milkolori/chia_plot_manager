7afd42b14f35548d8f7d739ced58a7d4ef5419c9
Milko Lorinkov <milkolori@gmail.com>
2021-05-17 09:31:53 +0300

    test

--- chianas/drive_manager.py
+++ chianas/drive_manager.py
@@ -615,7 +615,7 @@ def temperature_report():
     print(f'{blue}#    {nc}Serial#{blue}     #{nc}     Device{blue}     #{nc}     Drive{blue}     #{nc}    Temp{blue}     #{nc}')
     print(f'{blue}#################################################################{nc}')
     for drive in get_sorted_drive_list():
-        print(f'{blue}#{nc}   {str(Device(drive[1]).serial)}'f'{blue}     #{nc}'f'   {str(drive[1])}{blue}    #{nc}' f'    {((get_drive_by_mountpoint(str(drive[0]))))}{blue}    #{nc}' f'     {str(Device(drive[1]).temperature)}°C'f'{blue}     #{nc}')
+        print(f'{blue}#{nc}   {Device(drive[1]).serial}'f'{blue}')
     print(f'{blue}##################################################################{nc}')
     print('')
     print('')
