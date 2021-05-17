fc62d0f44fa445735ad6e281ca5768c758227940
Milko Lorinkov <milkolori@gmail.com>
2021-05-18 00:51:44 +0300

    test progress

--- chiaplot/send_plot.sh
+++ chiaplot/send_plot.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # 
 ssh pi@chiafarm01-internal -i /home/mmv/.ssh/id_rsa "nohup /home/pi/mining/plot_manager/receive_plot.sh $2 > foo.out 2> foo.err < /dev/null &"
-sudo /usr/bin/pv "$1" | sudo /usr/bin/nc -q 5 chiafarm01-internal 4040
+(sudo /usr/bin/pv "$1" -f | sudo /usr/bin/nc -q 5 chiafarm01-internal 4040) 2>&1
 exit
