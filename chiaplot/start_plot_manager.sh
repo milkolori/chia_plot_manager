ad1bb1fb1ac0b9f51d8eae1dcc2d7014330f8ef6
Milko Lorinkov <milkolori@gmail.com>
2021-05-19 19:02:05 +0300

    start plot manager

--- chiaplot/start_plot_manager.sh
+++ chiaplot/start_plot_manager.sh
@@ -1,1 +1,5 @@
-sudo python plot_manager.py > /home/mmv/mining/plot_manager/logs/runtime.log 2>&1 &
+if ps -ef | grep -v grep | grep plot_manager.py ; then
+	exit 0
+else
+	python /home/mmv/mining/plot_manager/code/chiaplot/plot_manager.py > /home/mmv/mining/plot_manager/logs/runtime.log 2>&1
+fi
